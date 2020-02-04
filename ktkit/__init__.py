199438fd155edfebf153e663f384f19c0967bcee
Sam Nicholls <samstudio8@gmail.com>
2020-02-04 13:32:26 +0000

    meow

--- ktkit/__init__.py
+++ ktkit/__init__.py
@@ -4,13 +4,36 @@ import os
 from .ncbiTaxonomyTree import NcbiTaxonomyTree
 
 def _load_dump(path):
-    tree = NcbiTaxonomyTree(nodes_filename=os.path.join(path, "nodes.dmp"), names_filename=os.path.join(path, "names.dmp"))
+    return NcbiTaxonomyTree(nodes_filename=os.path.join(path, "nodes.dmp"), names_filename=os.path.join(path, "names.dmp"))
+
+def _get_tid_for_rank(tree, rank, tid):
+    if tid == 0:
+        return 0
+
+    try:
+        lineage = tree.getAscendantsWithRanksAndNames([tid])[tid]
+    except KeyError:
+        sys.stderr.write("[WARN] Taxon ID %d not found in NCBI Dump\n" % tid)
+        return tid
+
+    for node in lineage:
+        if node.rank == rank:
+            return node.taxid
+    sys.stderr.write("Rank %s not found for taxon %d\n" % (rank, tid))
+    return 1
+
 
 def count(args):
     tree = _load_dump(args.dump)
 
+    mask = [_get_tid_for_rank(tree, args.rank, x) for x in args.mask]
+    mask.extend(args.mask)
+    mask.extend([0, 1])
+    sys.stderr.write("[NOTE] Masking: %s\n" % str(mask))
+
     counts = {}
     total_bp = 0
+    total_unmasked_bp = 0
     with open(args.input) as kh:
         for line in kh:
             fields = line.strip().split("\t")
@@ -19,6 +42,9 @@ def count(args):
             except ValueError:
                 # Get the taxid from the --use-names output
                 hit_tax = int(fields[2].split("taxid ")[1][:-1])
+
+            hit_tax = _get_tid_for_rank(tree, args.rank, hit_tax)
+
             hit_len = int(fields[3])
 
             if hit_tax not in counts:
@@ -26,11 +52,28 @@ def count(args):
             counts[hit_tax] += hit_len
             total_bp += hit_len
 
+            if hit_tax not in mask:
+                total_unmasked_bp += hit_len
+
     for tax_id in counts:
+
+        if tax_id == 0:
+            name = "unclassified"
+        else:
+            try:
+                name = tree.getName([tax_id])[tax_id].replace(" ", "_")
+            except KeyError:
+                name = tax_id
+
+        unmasked_total = 0
+        if tax_id not in mask:
+            unmasked_total = counts[tax_id]/total_unmasked_bp*100.0
         print("\t".join([str(x) for x in [
             tax_id,
+            name,
             counts[tax_id],
-            counts[tax_id]/total_bp*100.0
+            counts[tax_id]/total_bp*100.0,
+            unmasked_total,
         ]]))
 
 def check(args):
@@ -43,10 +86,13 @@ def cli():
     parser = argparse.ArgumentParser()
     parser.add_argument("mode")
     parser.add_argument("input")
+    parser.add_argument("--rank",help="Rank to output [default: species]", default="species")
     parser.add_argument("--dump", help="Path to NCBI taxonomy dump [default: ~/.ktkit/]", default="~/.ktkit")
+    parser.add_argument("--mask", help="NCBI Taxon IDs to suppress [default: 9606]", default="9606")
 
     args = parser.parse_args()
     args.dump = os.path.expanduser(args.dump)
+    args.mask = [int(x) for x in args.mask.split(",")]
 
     if not check(args):
         sys.stderr.write("NCBI dump not found in %s\n" % args.dump)
